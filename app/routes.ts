7506679b2a03d0604b00917a12c6ec5f055557c1
Tan Nhat <nguyentannhat246@gmail.com>
2025-07-31 15:25:11 +0700

    feat: base setup with auth

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from '@react-router/dev/routes'
+import { type RouteConfig, index, layout, route } from '@react-router/dev/routes'
 
-export default [index('routes/root/home.tsx')] satisfies RouteConfig
+export default [
+  layout('routes/auth/auth-layout.tsx', [
+    index('routes/root/home.tsx'),
+    route('sign-up', 'routes/auth/signup.tsx'),
+    route('sign-in', 'routes/auth/signin.tsx'),
+    route('forget-password', 'routes/auth/forget-password.tsx'),
+    route('reset-password', 'routes/auth/reset-password.tsx'),
+    route('verify-email', 'routes/auth/verify-email.tsx')
+  ])
+] satisfies RouteConfig
